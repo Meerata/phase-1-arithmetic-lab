357c271d49ce3a0215ba655211431a1628d2e772
Meerata <merata87@gmail.com>
2024-02-21 15:46:50 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Define variables num1 and num2
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Define variable random to generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define variables num3 and num4
+const num3 = 54;
+const num4 = 10;
+const mod = num3 % num4;
+
+// Define an array of numbers
+const numbers = [10, 20, 15, 5];
+const max = Math.max(...numbers);
+
+// Export variables for testing
+module.exports = { num1, num2, multiply, random, num3, num4, mod, max };
